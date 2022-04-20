422ee0d730414daf11679145736bb0f1517228f3
Linus Casassa <linus.casassa@rolls-royce.com>
2022-04-20 01:49:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from backend_framework_library import ServiceGenerator
 service = ServiceGenerator()
 app = service.app
 
-@service.add_route("/")
 def my_method():
     a = [1,2,3,4]
     df = pd.DataFrame(a)
     df2 = df.sort_values(df.columns[0])
     return df2.to_json(orient="values"),  200
+
+@service.add_route("/")
+def my_method_exposed():
+    my_method()
 
 
 if __name__ == "__main__":
