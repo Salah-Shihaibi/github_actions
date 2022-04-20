ea6dd2ac6f8fe5d56bf7c7f9fe2e6a7ba8b6323c
eobr <85750384+eobr@users.noreply.github.com>
2022-04-20 11:26:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from backend_framework_library import ServiceGenerator
 service = ServiceGenerator()
 app = service.app
 
-@service.add_route("/")
+
 def my_method():
     a = [1,2,3,4]
     df = pd.DataFrame(a)
     df2 = df.sort_values(df.columns[0])
     return df2.to_json(orient="values"),  200
+
+
+@service.add_route("/")
+def my_method_expose():
+    return my_method()
 
 
 if __name__ == "__main__":
