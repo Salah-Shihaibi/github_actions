421c0e387b849705761997366c7d6cf760435fab
Linus Casassa <linus.casassa@r2-factory.com>
2022-04-23 01:31:45 +0100

    return a joke

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from r2_factory_platform import ServiceGenerator
 service = ServiceGenerator()
 app = service.app
 
+
 def my_method():
-    a = [1,2,3,4]
-    df = pd.DataFrame(a)
-    df2 = df.sort_values(df.columns[0])
-    return df2.to_json(orient="values"),  200
+    return "Why was 6 afraid of 7? Because 7, 8, 9!"
+
 
 @service.add_route("/")
 def my_method_exposed():
@@ -67,7 +66,6 @@ def new_method2():
             description: User created
     """
     return json.dumps({"data": "example data"})
-
 
 
 if __name__ == "__main__":
